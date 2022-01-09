ffaf14bef55b8fe60b9be725e42636cd84c08e1a
nezu <29180158+dumbasPL@users.noreply.github.com>
2022-01-09 19:09:26 +0100

    implement http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,78 @@
-console.log('Hello World');
+import 'dotenv/config';
+import * as express from 'express';
+import * as morgan from 'morgan';
+import * as fsPath from 'path';
+import {createStorage} from './storage/storage';
+import {ImageProvider} from './storage/ImageProvider';
+
+(async () => {
+  const storage = await createStorage();
+  console.log(`Using ${storage.constructor.name} as storage backend`);
+
+  const imageProvider = new ImageProvider(storage);
+
+  const app = express();
+  app.use(express.json());
+
+  app.use(morgan(process.env.NODE_ENV == 'production' ? 'combined' : 'dev'));
+
+  // redirect to direct
+  app.get('/', (req, res) => res.redirect(301, '/direct'));
+
+  // redirect to the new redirect endpoint
+  app.get('/b/', (req, res) => res.redirect(301, '/redirect'));
+
+  // random image without redirect
+  app.get('/direct/:path(*)?', async (req, res, next) => {
+    try {
+      // get random image
+      const imagePath = await imageProvider.getRandomImagePath(req.params.path);
+
+      // get image data
+      const data = await imageProvider.getImageData(imagePath);
+
+      // set content type and pipe read stream to response
+      data.stream.pipe(res.status(200).type(data.type));
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  app.get('/redirect/:path(*)?', async (req, res, next) => {
+    try {
+      // get random image
+      const imagePath = await imageProvider.getRandomImagePath(req.params.path);
+
+      // redirect to the final image path
+      res.redirect(302, fsPath.join('/img/', imagePath));
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  app.get('/img/:path', async (req, res, next) => {
+    try {
+      // get image data
+      const data = await imageProvider.getImageData(req.params.path);
+
+      // set content type and pipe read stream to response
+      data.stream.pipe(res.status(200).type(data.type));
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  // generic error handler
+  app.use((error, req, res, next) => {
+    console.error(error);
+    return res.status(500).send(error.message ?? error.toString());
+  });
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+})().catch(error => {
+  console.error(error);
+
+  // force close if we get an exception while initialing
+  process.exit(1);
+});
