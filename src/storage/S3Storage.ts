281bd6ee5d2699f2a1cda64ec61655eaf43439f3
nezu <29180158+dumbasPL@users.noreply.github.com>
2022-01-09 19:08:54 +0100

    Add s3 storage support

---
+++ src/storage/S3Storage.ts
@@ -0,0 +1,44 @@
+import {Storage} from './storage';
+import {Client, ClientOptions} from 'minio';
+import {Readable} from 'stream';
+import * as fsPath from 'path';
+
+export class S3Storage implements Storage {
+
+  private client: Client;
+
+  private bucketName: string;
+
+  constructor(options: ClientOptions, bucketName: string) {
+    this.client = new Client(options);
+    this.bucketName = bucketName;
+  }
+
+  async createBucket() {
+    if (!await this.client.bucketExists(this.bucketName)) {
+      console.log(`Bucket "${this.bucketName}" does not exist, creating new bucket`);
+      this.client.makeBucket(this.bucketName, '');
+    }
+  }
+
+  private listFiles(path: string): Promise<Array<string>> {
+    return new Promise((resolve, reject) => {
+      const stream = this.client.listObjectsV2(this.bucketName, fsPath.relative('./', path) + '/');
+      const files: Array<string> = [];
+      stream.on('data', obj => files.push(obj.name));
+      stream.on('error', reject);
+      stream.on('end', () => resolve(files));
+    });
+  }
+
+  async getImages(path: string): Promise<string[]> {
+    return await this.listFiles(path);
+  }
+
+  async getImage(path: string): Promise<Readable> {
+    console.log(path);
+
+    return await this.client.getObject(this.bucketName, path);
+  }
+
+}
--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -1,5 +1,6 @@
 import {Readable} from 'stream';
 import {LocalStorage} from './localStorage';
+import {S3Storage} from './S3Storage';
 
 export interface Storage {
 
@@ -10,6 +11,31 @@ export interface Storage {
 }
 
 export async function createStorage(): Promise<Storage> {
+  if (process.env.S3_ENDPOINT) {
+    if (!Number.isInteger(parseInt(process.env.S3_PORT))) {
+      throw new Error(`S3_ENDPOINT is present but S3_PORT is missing/invalid`);
+    }
+    if (!process.env.S3_ACCESS_KEY) {
+      throw new Error(`S3_ENDPOINT is present but S3_ACCESS_KEY is missing`);
+    }
+    if (!process.env.S3_SECRET_KEY) {
+      throw new Error(`S3_ENDPOINT is present but S3_SECRET_KEY is missing`);
+    }
+    if (!process.env.S3_BUCKET_NAME) {
+      throw new Error(`S3_ENDPOINT is present but S3_BUCKET_NAME is missing`);
+    }
+    const s3Storage = new S3Storage({
+      endPoint: process.env.S3_ENDPOINT,
+      port: parseInt(process.env.S3_PORT),
+      useSSL: process.env.S3_USE_SSL == 'true',
+      accessKey: process.env.S3_ACCESS_KEY,
+      secretKey: process.env.S3_SECRET_KEY,
+      region: process.env.S3_REGION ? process.env.S3_REGION : undefined,
+    }, process.env.S3_BUCKET_NAME);
+    await s3Storage.createBucket();
+    return s3Storage;
+  }
+
   const dataDir = process.env.DATA_DIR;
   if (!dataDir) {
     throw new Error('DATA_DIR is missing');
